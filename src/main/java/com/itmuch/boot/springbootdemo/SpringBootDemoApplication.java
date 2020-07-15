d7c2bffc3c44f8f5cb6008fd8e2621ef33904540
wanghl <study_yz@163.com>
2020-07-15 20:46:28 +0800

    first commit

---
+++ src/main/java/com/itmuch/boot/springbootdemo/SpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.itmuch.boot.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+    // 启动类
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+
+}
